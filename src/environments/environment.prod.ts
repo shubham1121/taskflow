7132328c6210961ae39c8ab7e1ae34aba7fca232
Shubham Devrani <59960821+shubham1121@users.noreply.github.com>
2026-04-01 09:29:54 +0530

    chore: updated api-url to deployed backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.myapp.com/api',
+  apiBaseUrl: 'http://16.171.137.125:3000',
   userPollingFrequency: 10000, // Poll every 10 seconds
   tasksPollingFrequency: 5000, // Poll every 5 seconds
 };
