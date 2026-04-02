858e20e1e8e8886e9c2671d5ccee44c6a7b16f3f
Shubham Devrani <59960821+shubham1121@users.noreply.github.com>
2026-04-02 08:22:58 +0530

    fix: mobile view issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://16.171.137.125:3000',
+  apiBaseUrl: 'http://13.51.57.46:3000',
   userPollingFrequency: 10000, // Poll every 10 seconds
   tasksPollingFrequency: 5000, // Poll every 5 seconds
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000',
+  // apiBaseUrl: 'http://localhost:3000',
+    apiBaseUrl: 'http://13.51.57.46:3000',
   userPollingFrequency: 5000, // Poll every 5 seconds
   tasksPollingFrequency: 5000, // Poll every 5 seconds
 };
